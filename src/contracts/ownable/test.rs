720e6788e6d04a31a95b900f4f5170b07494515f
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:11:15 +0100

    Add Ownable unit tests with Soroban testutils

---
+++ src/contracts/ownable/test.rs
@@ -0,0 +1,16 @@
+#![cfg(test)]
+
+use super::*;
+use soroban_sdk::{testutils::Address as _, Address, Env};
+
+#[test]
+fn two_step_transfer() {
+    let env = Env::default();
+    let owner = Address::generate(&env);
+    let new_owner = Address::generate(&env);
+    env.mock_all_auths();
+    initialize(&env, &owner);
+    transfer_ownership(&env, &new_owner);
+    accept_ownership(&env);
+    assert_eq!(owner(&env), new_owner);
+}
