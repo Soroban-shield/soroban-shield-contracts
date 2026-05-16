f0b2e59b9f6cb1bd37fcdecb7c54e8ae2efd94b9
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:11:03 +0100

    Add contracts module registry

---
+++ src/contracts/mod.rs
@@ -0,0 +1,9 @@
+//! Composable Soroban contract modules.
+
+pub mod access_control;
+pub mod multi_sig;
+pub mod ownable;
+pub mod pausable;
+pub mod rate_limiter;
+pub mod reentrancy_guard;
+pub mod upgradeable;
