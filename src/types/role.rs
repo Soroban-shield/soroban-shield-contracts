e285daa29ce491e3d4dd7034825bfab34b9536b4
Chibey-max <ilorahdavid126@gmail.com>
2026-05-16 15:10:46 +0100

    Add generic role symbol helpers

---
+++ src/types/role.rs
@@ -0,0 +1,5 @@
+use soroban_sdk::Symbol;
+
+pub const ADMIN_ROLE: Symbol = soroban_sdk::symbol_short!("admin");
+pub const PAUSER_ROLE: Symbol = soroban_sdk::symbol_short!("pauser");
+pub const UPGRADER_ROLE: Symbol = soroban_sdk::symbol_short!("upgrdr");
